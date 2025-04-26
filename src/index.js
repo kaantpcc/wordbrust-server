22fe6fa8422759c84951a87eec21587cc3df5f76
Kaan Topcu <ktopcu106@gmail.com>
2025-04-26 05:12:01 +0300

    Register bug fix

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use("/auth", authRoutes);
+app.use("/api/auth", authRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
