82cfec9d4bdc5bbf9fb1e6de03820aa3a4ed4bd3
Kaan Topcu <ktopcu106@gmail.com>
2025-04-26 03:33:25 +0300

    Add Dockerfile and .dockerignore

--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
-const express = require('express');
-const path = require('path');
+const express = require("express");
+const path = require("path");
 require("dotenv").config({ path: path.resolve(__dirname, "../.env") });
 
 const app = express();
 const PORT = process.env.PORT;
 
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`)
-})
+  console.log(`Server is running on port ${PORT}`);
+});
