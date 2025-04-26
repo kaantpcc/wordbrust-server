ddc2f9da349abb368930811f08d358849bc25ba3
Kaan Topcu <ktopcu106@gmail.com>
2025-04-26 21:07:34 +0300

    Logout func fix

--- src/middleware/authMiddleware.js
+++ src/middleware/authMiddleware.js
@@ -27,4 +27,4 @@ const authenticateToken = (req, res, next) => {
   });
 };
 
-module.exports = authenticateToken;
+module.exports = { authenticateToken };
--- src/routes/AuthRoutes.js
+++ src/routes/AuthRoutes.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const AuthController = require("../controllers/AuthController.js");
 const router = express.Router();
-const authenticateToken = require("../middlewares/authMiddleware.js");
+const { authenticateToken } = require("../middlewares/authMiddleware.js");
 
 router.post("/register", AuthController.register);
 router.post("/login", AuthController.login);
