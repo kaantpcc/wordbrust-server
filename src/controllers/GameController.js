6b4a9ed8fdf51449824c3c2bf7aa97ffde0f1e4b
Kaan Topcu <ktopcu106@gmail.com>
2025-04-28 00:52:39 +0300

    Bug fix at socket.js and GameController.js

--- src/controllers/GameController.js
+++ src/controllers/GameController.js
@@ -1,6 +1,4 @@
 const GameService = require("../services/GameService");
-const BoardCells = require("../models/BoardCells");
-const { getIO } = require("../socket");
 
 class GameController {
   static async findOrCreateGame(req, res) {
@@ -13,34 +11,9 @@ class GameController {
       }
 
       const result = await GameService.findOrCreateGame(playerId, game_mode);
-      if (result.message === "Oyun bulundu") {
-        // Oyun bulundu ve eşleşme olduysa:
-        const board = await BoardCells.findAll({
-          where: { game_id: result.game.id },
-          attributes: [
-            "row",
-            "col",
-            "letter",
-            "letter_multiplier",
-            "word_multiplier",
-            "mine_type",
-            "bonus_type",
-          ],
-        });
-
-        const io = getIO();
-
-        // Odaya game_found mesajı gönderiyoruz
-        io.to(`game_${result.game.id}`).emit("game_found", {
-          message: "Rakibin geldi, oyun başlıyor!",
-          gameId: result.game.id,
-        });
-
-        // Odaya board_initialized mesajı gönderiyoruz
-        io.to(`game_${result.game.id}`).emit("board_initialized", board);
-      }
 
       res.status(200).json(result);
+
     } catch (error) {
       console.error("Error in findOrCreateGame:", error);
       res.status(500).json({ error: "Internal server error" });
--- src/socket.js
+++ src/socket.js
@@ -1,6 +1,8 @@
 const { Server } = require("socket.io");
+const BoardCells = require("./models/BoardCells"); // 🔥 boardu çekebilmek için ekliyoruz
 
 let io;
+const gameRooms = {}; // 🔥 Oda takibi için { gameId: userCount }
 
 function initSocket(server) {
   io = new Server(server, {
@@ -15,8 +17,41 @@ function initSocket(server) {
 
     socket.on("join_game_room", async ({ gameId }) => {
       try {
-        console.log(`User ${socket.id} joined game room: ${gameId}`);
+        console.log(`User ${socket.id} joining game room: ${gameId}`);
         socket.join(`game_${gameId}`);
+
+        // Oda kullanıcı sayısını güncelle
+        if (!gameRooms[gameId]) {
+          gameRooms[gameId] = 1;
+        } else {
+          gameRooms[gameId]++;
+        }
+
+        console.log(`Game ${gameId} current users: ${gameRooms[gameId]}`);
+
+        // Eğer odadaki kullanıcı sayısı 2 olduysa boardu gönder
+        if (gameRooms[gameId] === 2) {
+          console.log(`Game ${gameId} is ready! Sending board...`);
+
+          // Database'den boardu çekiyoruz
+          const board = await BoardCells.findAll({
+            where: { game_id: gameId },
+            attributes: [
+              "row",
+              "col",
+              "letter",
+              "letter_multiplier",
+              "word_multiplier",
+              "mine_type",
+              "bonus_type",
+            ],
+          });
+
+          io.to(`game_${gameId}`).emit("board_initialized", board);
+
+          console.log(`Board sent for game ${gameId}`);
+        }
+
       } catch (error) {
         console.log(`Error joining room: ${error}`);
       }
