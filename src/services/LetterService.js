451f4c94ca10db082b38b6bd3d978de86048ad63
Kaan Topcu <ktopcu106@gmail.com>
2025-05-01 18:32:26 +0300

    add sequelize to letterservice.js

--- src/services/LetterService.js
+++ src/services/LetterService.js
@@ -1,70 +1,74 @@
 const LettersPool = require("../models/LettersPool");
 const PlayerLetters = require("../models/PlayerLetters");
 const { Op } = require("sequelize");
+const sequelize = require("../config/database.js");
 
 class LetterService {
   static async giveInitialLettersToPlayer(gameId, playerId, count = 7) {
-    const pool = await LettersPool.findAll({
-      where: {
-        game_id: gameId,
-        remaining_count: {
-          [Op.gt]: 0,
+    return await sequelize.transaction(async (t) => {
+      const pool = await LettersPool.findAll({
+        where: {
+          game_id: gameId,
+          remaining_count: {
+            [Op.gt]: 0,
+          },
         },
-      },
-    });
+        transaction: t,
+        lock: t.LOCK.UPDATE, // havuzu kilitle
+      });
 
-    if (!pool.length) {
-      throw new Error("Harf havuzu boş.");
-    }
-
-    const letterBag = [];
-    for (const item of pool) {
-      for (let i = 0; i < item.remaining_count; i++) {
-        letterBag.push({ letter: item.letter });
+      if (!pool.length) {
+        throw new Error("Harf havuzu boş.");
       }
-    }
 
-    if (letterBag.length < count) {
-      throw new Error("Yeterli harf yok.");
-    }
+      const letterBag = [];
+      for (const item of pool) {
+        for (let i = 0; i < item.remaining_count; i++) {
+          letterBag.push({ letter: item.letter });
+        }
+      }
 
-    // 7 rastgele harf
-    const selectedLetters = [];
-    for (let i = 0; i < count; i++) {
-      const randIndex = Math.floor(Math.random() * letterBag.length);
-      const chosenLetter = letterBag[randIndex];
-      selectedLetters.push(chosenLetter);
-      letterBag.splice(randIndex, 1);
-    }
+      if (letterBag.length < count) {
+        throw new Error("Yeterli harf yok.");
+      }
 
-    //kullanıcı harfleri
-    await PlayerLetters.bulkCreate(
-      selectedLetters.map((l) => ({
-        game_id: gameId,
-        player_id: playerId,
-        letter: l.letter,
-        is_frozen: false,
-      }))
-    );
+      const selectedLetters = [];
+      for (let i = 0; i < count; i++) {
+        const randIndex = Math.floor(Math.random() * letterBag.length);
+        const chosenLetter = letterBag[randIndex];
+        selectedLetters.push(chosenLetter);
+        letterBag.splice(randIndex, 1);
+      }
 
-    // havuzdan düşür
-    const usageMap = {};
-    for (const { letter } of selectedLetters) {
-      usageMap[letter] = (usageMap[letter] || 0) + 1;
-    }
+      await PlayerLetters.bulkCreate(
+        selectedLetters.map((l) => ({
+          game_id: gameId,
+          player_id: playerId,
+          letter: l.letter,
+          is_frozen: false,
+        })),
+        { transaction: t }
+      );
 
-    await Promise.all(
-      Object.entries(usageMap).map(([letter, amount]) =>
-        LettersPool.decrement("remaining_count", {
-          by: amount,
-          where: { game_id: gameId, letter },
-        })
-      )
-    );
+      const usageMap = {};
+      for (const { letter } of selectedLetters) {
+        usageMap[letter] = (usageMap[letter] || 0) + 1;
+      }
 
-    return {
-      letters: selectedLetters,
-    }; // [{ letter: "A" }, ...]
+      await Promise.all(
+        Object.entries(usageMap).map(([letter, amount]) =>
+          LettersPool.decrement("remaining_count", {
+            by: amount,
+            where: { game_id: gameId, letter },
+            transaction: t,
+          })
+        )
+      );
+
+      return {
+        letters: selectedLetters,
+      };
+    });
   }
 }
 
